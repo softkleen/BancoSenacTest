982dcbe48843b2ff127d9ea96ebf45fa39667d13
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2024-10-30 08:25:01 -0300

    Adicionar arquivos de projeto.

---
+++ Banco/ContaBancaria.cs
@@ -0,0 +1,52 @@
+﻿namespace Banco
+{
+    public class ContaBancaria
+    {
+        private readonly string m_nomeCliente;
+        private double m_saldo;
+
+
+
+        private ContaBancaria() { }
+        private ContaBancaria(string nomeCliente, double saldo) 
+        {
+            m_nomeCliente = nomeCliente;
+            m_saldo = saldo;
+        }
+
+        public string NomeCliente { get { return m_nomeCliente; } }
+        public double Saldo { get{ return m_saldo; } }
+
+
+        public void Debitar(double montante)
+        { 
+            if(montante > m_saldo) 
+            {
+                throw new ArgumentOutOfRangeException("montante");
+            }
+            if (montante < 0)
+            {
+                throw new ArgumentOutOfRangeException("montante");
+            }
+            m_saldo += montante; // errado intencionalmente
+        }
+        public void Creditar(double montante)
+        {
+            if (montante < 0)
+            {
+                throw new ArgumentOutOfRangeException("montante");
+            }
+            m_saldo += montante; 
+        }
+
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new("James MaCgregor", 11.99);
+            conta.Creditar(5.77);
+            conta.Debitar(11.22);
+            Console.WriteLine($"O Saldo atual é R$ {conta.Saldo}");
+
+           
+        }
+    }
+}
---
+++ BancoTestes/UnitTest1.cs
@@ -0,0 +1,12 @@
+
+namespace BancoTestes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
